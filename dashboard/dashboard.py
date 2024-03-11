542fb6171cf752a61d02a12c5e94f35e8ddbcacf
Salim Husaini <159422377+salimhu@users.noreply.github.com>
2024-03-11 10:04:01 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,10 +5,10 @@ import seaborn as sns
 
 # Load the dataset
 df = pd.read_csv("dashboard/day.csv")
-df['dteday'] = pd.to_datetime(df['dteday'])
+df['date'] = pd.to_datetime(df['date'])
 
 # Streamlit App
-st.title("Bike Sharing Dashboard")
+st.title("Sepedah Rodalink Dashboard")
 
 # Add sidebar
 st.sidebar.header("Dashboard Sepedah")
@@ -27,27 +27,27 @@ st.sidebar.image(image_path, caption="Sepedah", use_column_width=True)
 
 # Add filters to the sidebar
 selected_season = st.sidebar.selectbox("Select Season", sorted(df['season'].unique()))
-selected_month = st.sidebar.selectbox("Select Mnth", sorted(df['mnth'].unique()))
+selected_month = st.sidebar.selectbox("Select Month", sorted(df['month'].unique()))
 selected_weather = st.sidebar.selectbox("Select Weather Situation", sorted(df['weathersit'].unique()))
 
 # Filter the data based on selected options
-filtered_data = df[(df['season'] == selected_season) & (df['mnth'] == selected_month) & (df['weathersit'] == selected_weather)]
+filtered_data = df[(df['season'] == selected_season) & (df['month'] == selected_month) & (df['weathersit'] == selected_weather)]
 
 # Scatter plot for temperature vs. bike counts
-st.subheader("Scatter Plot: Temperature vs. Bike Counts")
+st.subheader("Sepedah and Temperature")
 fig, ax = plt.subplots(figsize=(8, 6))
-sns.histplot(x='temp', y='cnt', data=df, ax=ax)
+sns.histplot(x='temp', y='count', data=df, ax=ax)
 st.pyplot(fig)
 
 # Line chart for daily bike counts
-st.subheader("Daily Bike Counts Over Time")
+st.subheader("Sepedah Over Time")
 fig, ax = plt.subplots(figsize=(10, 6))
-sns.barplot(x='dteday', y='cnt', data=df, ax=ax)
+sns.barplot(x='date', y='count', data=df, ax=ax)
 st.pyplot(fig)
 
 # Bar chart for bike counts by season
-st.subheader("Bike Counts by Season")
-season_counts = df.groupby('season')['cnt'].sum()
+st.subheader("Sepedah by Season")
+season_counts = df.groupby('season')['count'].sum()
 fig, ax = plt.subplots()
 season_counts.plot(kind='bar', ax=ax)
 st.pyplot(fig)
